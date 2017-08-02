ee6eae7de9b78c7742d2b3176a3891ed602c1ec5
guillaume <guillaume.chausse@live.fr>
2017-08-02 15:09:05 +0200

    add create/delete table

--- classes/GC_Testimonials.php
+++ classes/GC_Testimonials.php
@@ -16,13 +16,15 @@ class GC_Testimonials extends Module{
 
 	    $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
 
-	    if (!Configuration::get('Title'))
+	    if (!Configuration::get('Title')){
 	       $this->warning = $this->l('No name provided');
+        }
 	}
 
 	public function install(){
-        if (Shop::isFeatureActive()){
-        Shop::setContext(Shop::CONTEXT_ALL);
+        if (Shop::isFeatureActive())
+        {
+            Shop::setContext(Shop::CONTEXT_ALL);
         }
         $parent_tab = new Tab();
         $parent_tab->name[$this->context->language->id] = $this->l('Testimonials tab');
@@ -31,36 +33,39 @@ class GC_Testimonials extends Module{
         $parent_tab->module = $this->name;
         $parent_tab->add();
 
-        if (!parent::install()
-            || !$this->registerHook('admingc')
-            || !$this->registerHook('home')
-            || !$this->registerHook('header')
-            || !Configuration::updateValue('GC_TESTIMONIALSMODULE_NAME', 'world')
-        ) {
-        return false;
-       }
+        if (!parent::install()|| !$this->installDb()) 
+        {
+            return false;
+        }
        
     }
 
-    public function hookadmingc(){        
-        $this->smarty->assign(array(
-           'ciao' => 'hola'
-       ));
-       return $this->display(__FILE__, 'Admin.tpl');
+    public  function installDb()
+
+    {
+        $sql = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'testimonials` (
+            `id_testimonials` int(11) NOT NULL AUTO_INCREMENT,
+            `testimonials_name` varchar(50) NOT NULL,
+            `testimonials_description` varchar(200) NOT NULL,
+            `date_testimonials` date NOT NULL,
+            PRIMARY KEY (`id_testimonials`))';
+        return Db::getInstance()->execute($sql);
     }
      
-	public function uninstall(){
+	public function uninstall()
+    {
         $tab = new Tab((int)Tab::getIdFromClassName('Admintestimonials'));
         $tab->delete();
-	  if (!parent::uninstall()
-        || !Configuration::deleteByName('testimonials_NAME')
-
-      )
-	    return false;
+	    if (!parent::uninstall()|| !$this->uninstallDb()){
+	        return false;
+        }
 	    return true;
 	}
-
     
-   
+     public function uninstallDb()
+    {
+        $sql = 'DROP TABLE '._DB_PREFIX_.'testimonials';
+        Db::getInstance()->execute($sql);
+        return true;
+    }
 }
-?>
